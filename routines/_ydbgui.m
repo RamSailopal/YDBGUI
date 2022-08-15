58e7b87a4e410a0f161a7a26b5bb22679152ccb6
Sam Habiel <sam@yottadb.com>
2022-08-15 13:18:29 -0400

    [#183] Remove M-Web-Server compile, install logic, delegate to M-Web-Server repo
    Commit
    https://github.com/shabiel/M-Web-Server/commit/45f50be210d82f7516c501a69067f046d5750eab
    refactored the CMake files so that all installation for the M-Web-Server is handled by the
    top level CMakeLists.txt in that project, and we only reference the
    project as an external project using
    FetchContent_Declare/FetchContent_MakeAvailable.
    
    There will be future work on this, but this change is sound by itself,
    as it lays down the infrastructure of how we will manage plug-in
    dependencies using CMake.
    
    How you compile/install the YottaDB GUI does not change with this
    commit.
    
    This commit also refactors the start/stop logic to be more robust,
    including port open checking, and stoping an existing process when
    starting a server at the same port.

--- routines/_ydbgui.m
+++ routines/_ydbgui.m
@@ -14,7 +14,9 @@
 job	; [Public, Default] Non-blocking entry point (server runs in a jobbed off process)
 	new options do cmdline(.options)
 	if '$data(options("port")) set options("port")=8089
+	do portIsOpen(options("port"))
 	do setzd
+	;
 	; params: port, tlsconfig, nogblmode, user:pass, nogzip
 	do job^%webreq(options("port"),"",1,"",1)
 	quit
@@ -25,15 +27,21 @@ start	; [Public] Blocking entry point (listener runs in the foreground)
 	use $principal:(ctrap=$char(3):nocenable:exception="halt")
 	new options do cmdline(.options)
 	if '$data(options("port")) set options("port")=8089
+	do portIsOpen(options("port"))
 	do setzd
+	;
 	; params: port, debug, tlsconfig, nogbl, trace, user:pass, nogzip
 	do start^%webreq(options("port"),0,"",1,0,"",1)
 	quit
 	;
 stop	; [Public] Stop Web Server
 	; Find PID first using fuser
-	new pids,pid,fuserOutput
-	do runShell^%ydbguiUtils("fuser $ydb_dist/plugin/o/_ydbgui.so",.fuserOutput)
+	new pids,pid,fuserOutput,shellStatus
+	new utf8subdir set utf8subdir=$select($zchset="UTF-8":"utf8/",1:"")
+	set shellStatus=$$runShell^%ydbguiUtils("fuser $ydb_dist/plugin/o/"_utf8subdir_"_ydbgui.so",.fuserOutput)
+	if shellStatus=1 write "`fuser` reports no web server is running",! quit
+	else  if shellStatus>1 write "Failed to run fuser",! quit
+	;
 	set pids=$piece(fuserOutput(1),": ",2)
 	;
 	new i for i=1:1:$length(pids," ") set pid=$piece(pids," ",i) do
@@ -41,7 +49,8 @@ stop	; [Public] Stop Web Server
 	. if pid=$job quit  ; this is us. Don't terminate self.
 	. ;
 	. new mupipOutput
-	. do runShell^%ydbguiUtils("$ydb_dist/mupip stop "_pid,.mupipOutput)
+	. set shellStatus=$$runShell^%ydbguiUtils("$ydb_dist/mupip stop "_pid,.mupipOutput)
+	. if shellStatus write "Failed to run mupip stop",! quit
 	. write mupipOutput(1),!
 	quit
 	;
@@ -65,4 +74,44 @@ cmdline(options) ; [Private] Process command line options
 	. if $$trimleadingstr^%XCMD(.cmdline,"port") do
 	.. do trimleadingstr^%XCMD(.cmdline," ")
 	.. set options("port")=cmdline
+	quit
+	;
+portIsOpen(port) ; [$$ Private] Check if port is open
+	open "porttest":(connect="127.0.0.1:"_options("port")_":TCP":delim=$char(13,10):attach="client"):0:"SOCKET"
+	if $test do  quit
+	. write "Port "_options("port")_" is currently being used.",!
+	. write "Checking if it is the YDBGUI.",!
+	. ;
+	. use "porttest"
+	. write "GET /ping HTTP/1.1"_$char(13,10)
+	. write "Host: localhost:"_options("port")_$char(13,10)
+	. write "User-Agent: "_$zposition_$char(13,10)
+	. write "Accept: */*"_$char(13,10)_$char(13,10)
+	. new httpstatus read httpstatus
+	. use $principal
+	. write httpstatus,!
+	. use "porttest"
+	. new body 
+	. do  close "porttest"
+	. . if httpstatus'["200 OK" use $principal write "Not a YottaDB GUI Server",! quit
+	. . new i for i=1:1 read header(i) quit:header(i)=""  set headerByType($piece(header(i),": "))=$piece(header(i),": ",2,99)
+	. . if '$data(headerByType("Content-Length")) use $principal write "No Content-Length header",! quit
+	. . read body#headerByType("Content-Length"):0
+	. use $principal
+	. write body,!
+	. new parsedBody,error do decode^%webjson($na(body),$na(parsedBody),$na(error))
+	. if $data(error) write "Error parsing Web Server response",!
+	. ;
+	. write "YDBGUI Server is currently running",!
+	. new shellStatus,fuserOutput
+	. set shellStatus=$$runShell^%ydbguiUtils("fuser "_options("port")_"/tcp",.fuserOutput)
+	. if shellStatus=1 write "Something went wrong... Contact YottaDB Support.",! quit
+	. new pid set pid=$$FUNC^%TRIM($piece(fuserOutput(1),":",2))
+	. write "fuser says that this is the PID: "_pid,!
+	. ;
+	. write "Now going to stop it...",!
+	. new mupipOutput
+	. set shellStatus=$$runShell^%ydbguiUtils("$ydb_dist/mupip stop "_pid,.mupipOutput)
+	. if shellStatus write "Failed to run mupip stop",! quit
+	. write mupipOutput(1),!
 	quit
