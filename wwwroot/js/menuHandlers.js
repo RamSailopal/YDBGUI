4f1e7d58e90d9ad74d9e00036015415bd5f05b04
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-10-19 11:08:20 +0100

    Adding changes for restart

--- wwwroot/js/menuHandlers.js
+++ wwwroot/js/menuHandlers.js
@@ -22,7 +22,7 @@ app.ui.menu.init = () => {
     $('#menuSystemAdministrationRegionAddLi').on('click', () => app.ui.regionAdd.name.show());
     $('#menuSystemAdministrationRegionAdd').on('click', () => app.ui.regionAdd.name.show());
     $('#menuSystemAdministrationLocksManagerLi').on('click', () => app.ui.locksManager.show());
-    $('#menuRestartManagerLi').on('click', () => app.ui.locksManager.show());
+    $('#menuRestartManagerLi').on('click', () => app.ui.RestartManager.show());
 
     // documentation
     $('#menuDocumentationAdministration').on('click', () => window.open('https://docs.yottadb.com/AdminOpsGuide/index.html', '_blank'));
