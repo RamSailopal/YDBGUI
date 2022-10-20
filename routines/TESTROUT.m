8a02362cf5e25190e611d789eb68c80b7d2bbc06
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-10-20 10:12:36 +0100

    Still some references to old ^SYS global

--- routines/TESTROUT.m
+++ routines/TESTROUT.m
@@ -2,9 +2,9 @@ TESTROUT ;Test routine for system startup
 JOB ;
 	Job RESTART
 RESTART ;
-    Q:SYS("restart-status")="restarting"
+    Q:$G(^GUISYS("restart-status"))="restarting"
     Hang 120
-	Set ^SYS("restart-status")="restarted"
+	Set ^GUISYS("restart-status")="restarted"
     Set DateTime=$zdate($h,"MON DD YYYY/12:60:SS")
     Set Date=$Piece(DateTime,"/",1)
 	Set Time=$Piece(DateTime,"/",2)
