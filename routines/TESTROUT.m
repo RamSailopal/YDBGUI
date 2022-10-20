48c911c9269aecd253953df3bf093b9d21bdadea
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-10-20 15:20:36 +0100

    Moved restart-status set

--- routines/TESTROUT.m
+++ routines/TESTROUT.m
@@ -5,13 +5,13 @@ JOB ;
     Quit
 RESTART ;
     Q:$G(^GUISYS("restart-status"))="restarting"
-    S ^GUISYS("restart-status")="restarting"
+    Set ^GUISYS("restart-status")="restarting"
     Hang 120
-	Set ^GUISYS("restart-status")="restarted"
     Set DateTime=$zdate($h,"MON DD YYYY/12:60:SS")
     Set Date=$Piece(DateTime,"/",1)
 	Set Time=$Piece(DateTime,"/",2)
     Set ^GUISYS("restart-date")=Date
 	Set ^GUISYS("restart-time")=Time
     Set ^GUISYS("restart-process")="Finished"
-	Quit
+	Set ^GUISYS("restart-status")="restarted"
+    Quit
